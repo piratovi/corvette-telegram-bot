9d86bb562984e82ce9096255e0f08e6ba2f6f1c5
pirat <piratovi@gmail.com>
2024-12-08 08:15:07 +0700

    init commit

---
+++ src/main/java/com/kolosov/corvettetelegrambot/CorvetteTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.kolosov.corvettetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorvetteTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorvetteTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kolosov/corvettetelegrambot/CorvetteTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolosov.corvettetelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorvetteTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
